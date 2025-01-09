7218de46400a0ff91b97c8dc750306928ccc1c04
Giacinto Boccia <83920369+Gianfilippo980@users.noreply.github.com>
2025-01-10 00:13:42 +0100

    Create Telemastodon.py
    Initial commit

---
+++ Telemastodon.py
@@ -0,0 +1,63 @@
+"""Bot per il caricamento su Mastodon dell'immagine dell'ultimora del Televideo"""
+
+import requests
+from PIL import Image
+from io import BytesIO
+import feedparser
+from time import sleep
+from mastodon import Mastodon
+
+#Gestione immagine
+indirizzo_immagine = "https://www.televideo.rai.it/televideo/pub/tt4web/Nazionale/16_9_page-101.png"
+indirizzo_feed = 'https://www.televideo.rai.it/televideo/pub/rss101.xml'
+
+def scarica_immagine(url):
+    try:
+        risposta = requests.get(url)
+        risposta.raise_for_status()
+        immagine = Image.open(BytesIO(risposta.content))
+        return immagine
+    except:
+        print("Errore di rete")
+        return None
+
+def apri_rss(url_rss):
+    try:
+        feed = feedparser.parse(url_rss)
+        return feed
+    except:
+        print("Errore RSS")
+        return None
+
+def ora_ultima(feed):
+    return feed.entries[0].published_parsed
+
+#Gestione Mastodon
+mastodon = Mastodon(client_id = 'Telepython_client.secret')
+mastodon.log_in([AUTENTICATION], to_file= 'Telepython_utente.secret', scopes=['write'])
+
+def posta_immagine(rss):
+    media = mastodon.media_post('ultim_ora.png', description= rss.summary)
+    titolo = "#Ultimora " + rss.title
+    mastodon.status_post(titolo, media_ids= media, language= 'IT')
+
+#Loop
+ora_precedente = None
+while True:
+    feed_rss = apri_rss(indirizzo_feed)
+    if feed_rss is None:
+        sleep(10)
+        continue
+    sleep(20)
+    #Ritardo qui per essere sicuro di prendere l'immagine dopo l'aggiornamento
+
+    ora_corrente = ora_ultima(feed_rss)    
+    if ora_corrente != ora_precedente:
+        print("Ora cambiata.")
+        immagine_corrente = scarica_immagine(indirizzo_immagine)
+        if immagine_corrente is None:
+            #In caso di errore attende un altro ciclo
+            continue
+        ora_precedente = ora_corrente
+        immagine_corrente.save("ultim_ora.png")
+        posta_immagine(feed_rss.entries[0])
