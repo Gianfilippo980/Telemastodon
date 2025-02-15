7a38e501fa77b8cc0b75cf7f00bbf1b402a7c083
Gianfilippo980 <giacinto.boccia@outlook.it>
2025-02-15 11:37:51 +0100

    Definizione metodi per RSS e Immagine

--- Telemastodon.py
+++ Telemastodon.py
@@ -4,9 +4,10 @@ import requests
 import feedparser
 import time
 import threading
-from Credenziali import mastodon
+from Credenziali import mastodon as credenziali_mastodon
 from PIL import Image
 from io import BytesIO
+from hashlib import md5 as hasher
 from mastodon import Mastodon
 
 #Indirizzi
@@ -22,52 +23,87 @@ finestra_rss = 60
 finestra_immagine = 60
 
 class RSS:
-    def __init__(self, intervallo : int, indirizzo:str):
+    def __init__(self, intervallo_controlli : int, finestra_novità : int, indirizzo : str) -> None:
         self.indirizzo = indirizzo
-        self.intervallo = intervallo
+        self.intervallo = intervallo_controlli
+        self.finestra = finestra_novità
+        self.ora_ultimo_cambio = time.gmtime()
         self._stop = threading.Event()
         self.thread = threading.Thread(target= self._ciclo)
         self.thread.start()
-        self.feed = None
-        self.scorsa_ora = time.gmtime()
-    def _ciclo(self):
+
+    def _ciclo(self) -> None:
         while not self._stop.is_set():
             self.aggiorna()
-            time.sleep(self.intervallo) 
-    def aggiorna(self):
-        try:
-            self.feed = feedparser.parse(self.indirizzo)
-        except:
-            return
-        
+            time.sleep(self.intervallo)
 
+    def aggiorna(self) -> None:
+        try:
+            self.lacnio = feedparser.parse(self.indirizzo).entries[0]
+            ora_corrente = self.lancio.published_parsed
+            if ora_corrente > self.ora_ultimo_cambio:
+                self.ora_ultimo_cambio = ora_corrente
+        except:
+            print("Errore RSS")
+
+    def titolo(self) -> str | None:
+        #Restituisce il titolo dell'ultimo lancio RSS scaricato
+        try:
+            return self.lancio.title + "\n\n" + "#Ultimora"
+        except:
+            return None 
+        
+    def descrizione(self) -> str | None:
+        #Restituisce il sommario dell'ultimo lancio RSS scaricato
+        try:
+            return self.lancio.summary
+        except:
+            return None
+        
+    def se_nuovo(self) -> bool:
+        #Restituisce True se il lancio RSS è stato aggiornato entro la finestra di tempo
+        return time.gmtime() - self.ora_ultimo_cambio < self.finestra
 
 class Immagine:
-    def __init__(self, indirizzo : str):
+    def __init__(self, indirizzo : str, intervallo_controlli : int, finestra_novità : int) -> None:
         self.indirizzo = indirizzo
-    def scarica_immagine(url):
+        self.ora_ultimo_cambio = time.gmtime()
+        self.intervallo = intervallo_controlli
+        self.finestra = finestra_novità
+        self._stop = threading.Event()
+        self.thread = threading.Thread(target= self._ciclo)
+        self.thread.start()
+        self.immagine = None
+
+    def _ciclo(self) -> None:
+        while not self._stop.is_set():
+            self.aggiorna()
+            time.sleep(self.intervallo)
+
+    def scarica_immagine(url) -> Image.Image | None:
         try:
             risposta = requests.get(url)
             risposta.raise_for_status()
             immagine = Image.open(BytesIO(risposta.content))
             return immagine
         except:
-            print("Errore di rete")
+            print("Errore immagine")
             return None
+    
+    def aggiorna(self) -> None:
+        nuova_immagine = self.scarica_immagine(self.indirizzo)
+        if nuova_immagine is not None:
+            self.ora_ultimo_cambio = time.gmtime()
+            self.immagine = nuova_immagine
 
-def apri_rss(url_rss):
-    try:
-        feed = feedparser.parse(url_rss)
-        return feed
-    except:
-        print("Errore RSS")
-        return None
-
-def ora_ultima(feed):
-    try:
-        return feed.entries[0].published_parsed
-    except:
-        return None
+    def se_nuovo(self) -> bool:
+        return time.gmtime() - self.ora_ultimo_cambio < self.finestra
+    
+    def immagine(self) -> Image.Image | None:
+        try:
+            return self.immagine
+        except:
+            return None
 
 #Gestione Mastodon
 mastodon = Mastodon(client_id = 'Telepython_client.secret')
