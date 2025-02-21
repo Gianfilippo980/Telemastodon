89c1fd6b080e98a6baf9d4d82dfe7abe65695efb
Giacinto Boccia <giacinto.boccia@outlook.it>
2025-02-21 15:37:30 +0100

    Prove OCR immagine

---
+++ Tester_OCR.py
@@ -0,0 +1,53 @@
+"""Programma per verificare il funzionamento dell'OCR sul televideo"""
+
+import pytesseract
+from PIL import Image
+import time
+import requests
+from io import BytesIO
+import re
+
+indirizzo_immagine = 'https://www.televideo.rai.it/televideo/pub/tt4web/Nazionale/16_9_page-101.png'
+
+def scarica_immagine(indirizzo : str) -> Image.Image | None:
+    try:
+        risposta = requests.get(indirizzo)
+        risposta.raise_for_status()
+        immagine = Image.open(BytesIO(risposta.content))
+        return immagine
+    except:
+        print("Errore immagine")
+        return None
+    
+def riconosci_testo(immagine : Image.Image) -> str:
+    zona_orario = immagine.crop((22, 24, 121, 55))
+    testo = pytesseract.image_to_string(zona_orario, config='--psm 7')
+    testo = re.sub(r'[^0-9.]', '', testo)
+    return testo
+
+def orario(testo: str) -> time.struct_time | None:
+    try:
+        ora = time.strptime(testo, "%H.%M")
+        return ora
+    except:
+        print("Errore orario")
+        return None
+    
+def main() -> None:
+    testo = ''
+    ora = None
+    while True:
+        immagine = scarica_immagine(indirizzo_immagine)
+        if immagine is not None:
+            nuovo_testo = riconosci_testo(immagine)
+            nuova_ora = orario(testo)
+            if nuovo_testo != testo:
+                print ("Nuovo testo:", nuovo_testo)
+                testo = nuovo_testo
+            if nuova_ora != ora:
+                print("Nuova ora:", nuova_ora)
+                ora = nuova_ora
+        time.sleep(20)
+        
+if __name__ == '__main__':
+    main()
